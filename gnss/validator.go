c8d30c0630bc4c54dbd14f0751c75f7b9b2ea2a8
David Biro <75266347+david-biro@users.noreply.github.com>
2026-01-10 13:06:07 +0100

    changed validator

--- gnss/validator.go
+++ gnss/validator.go
@@ -1,30 +1,37 @@
 package gnss
 
 import (
-	"strconv"
-	"strings"
+        //"strconv"
+        //"strings"
+        "regexp"
 )
 
+var coordinate = regexp.MustCompile(`^-?\d{1,3}(?:\.\d+)?$`)
+
+
 func IsDottedDecimal(lat, lon string) bool {
-	return isDottedDecimal(lat) && isDottedDecimal(lon)
+        return isDottedDecimal(lat) && isDottedDecimal(lon)
 }
 
 func isDottedDecimal(s string) bool {
-	if s == "" {
-		return false
-	}
+        //if s == "" {
+        //      return false
+        //}
 
-	// when no position data is coming (unlikely to be exactly there :) )
-	if s == "0.0" {
-		return false
-	}
+        // when no position data is coming (unlikely to be exactly there :) )
+        if s == "0.0" {
+                return false
+        }
 
-	// must contain a decimal point
-	if !strings.Contains(s, ".") {
-		return false
-	}
 
-	// must be a valid number
-	_, err := strconv.ParseFloat(s, 64)
-	return err == nil
+        return coordinate.MatchString(s)
+
+        // must contain a decimal point
+        //if !strings.Contains(s, ".") {
+        //      return false
+        //}
+
+        // must be a valid number
+        //_, err := strconv.ParseFloat(s, 64)
+        //return err == nil
 }
